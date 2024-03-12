d06cb1749d61a56c12bbd598bf272fe4802e4094
laleebozkurtt <162982040+laleebozkurtt@users.noreply.github.com>
2024-03-12 19:43:24 +0300

    Update Aboutpage.jsx

--- src/components/Aboutpage.jsx
+++ src/components/Aboutpage.jsx
@@ -14,7 +14,7 @@ function Aboutpage() {
                 <Row className='textbackground'>
                     <Col md={7} >
                         <h3 className='aboutmetext'>About <span>Me</span></h3>
-                        <p className='aboutdetails'> I m a self-taught Front End developer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
+                        <p className='aboutdetails'> Hi! I am Lale Bozkurt, a software engineering student. During my undergraduate education, I worked in the field of image processing in the Autonomous Vehicle Race organized by Teknofest. During this process, I gained experience in teamwork outside of software. Additionally, I took part in the management of school and out-of-school communities. I graduated from the recently held Turkcell Software Testing Expertise program.
                         </p>
                         <div className="skillsContainer">
                             <div className="skill--scroll">
